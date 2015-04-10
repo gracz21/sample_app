4f6aa16cc3cff3c86076bb35372f5b4332017a78
Kamil Walkowiak <kamil.walek21@gmail.com>
2015-04-10 16:39:14 +0000

    Finish layouts and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'user/new'
+
   root 'static_pages#home'
-  get 'static_pages/help'
-  get 'static_pages/about'
+  get 'help' => 'static_pages#help'
+  get 'about' => 'static_pages#about'
+  get 'contact' => 'static_pages#contact'
+  get 'signup' => 'user#new'
 
 end
